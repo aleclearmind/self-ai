5f68c6c74e1a254e76c0d47cd5fcda574bb195d8
Alessandro Di Federico <ale@clearmind.me>
2026-03-01 21:42:01 +0100

    Switch to nix on the target machine

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     simple-uvnix.url = "github:aleclearmind/simple-uvnix";
     simple-uvnix.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -29,6 +29,21 @@
         in
         {
           default = devShell;
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          ollama = pkgs.writeShellScriptBin "ollama" ''
+            exec nix run --override-input nixpkgs nixpkgs/nixos-25.11 --impure github:aleclearmind/nixGL/8b4cf8637c0b0bdbe433a8758395f8ee58148c54 -- ${pkgs.ollama-cuda}/bin/ollama "$@"
+          '';
         }
       );
 
